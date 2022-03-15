41031309351c4c73e3516c521f1ccf6b85bb7a6f
sabanna3838 <sabanna3838@gmail.com>
2022-03-16 01:56:21 +0900

    Update application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  # 追加
-  config.serve_static_assets = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
